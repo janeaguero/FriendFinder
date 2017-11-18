851482f0b7798366908b40adea7308bc691b3073
Jane Aguero <jane.m.aguero@gmail.com>
2017-11-18 11:27:41 -0500

    general work and updates

---
+++ data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+  {
+    name: "Smudge",
+    photo: "https://qph.ec.quoracdn.net/main-qimg-26f54c4668147dfedc82e9a157719b27-c",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Tetra",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Bicolor_he-cat.JPG/320px-Bicolor_he-cat.JPG",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Rogue",
+    photo: "https://www.siamesecatspot.com/wp-content/uploads/2014/03/simalee1.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Tigger",
+    photo: "https://savannahcatbreed.com/wp-content/uploads/2014/03/EliteNewNotSat-300x300-1.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Pumpkin",
+    photo: "https://cdn.shopify.com/s/files/1/0344/6469/files/Screen_Shot_2016-10-04_at_11.15.12_AM_large.png?v=1475594859",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Grey Stripe",
+    photo: "https://i.pinimg.com/736x/3d/0a/ca/3d0aca83860acc129933217f2d2f7c91--tabby-cats-kitty-cats.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
